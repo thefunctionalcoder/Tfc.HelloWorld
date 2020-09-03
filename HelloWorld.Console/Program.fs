5efcde434e87646c2e28c92c9d6ede3f4670b9ec
TheFunctionalCoder <aaron@thefunctionalcoder.com>
2020-09-02 20:17:55 -0400

    Hello World Code

---
+++ HelloWorld.Console/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// %s string
+// %d some form of int
+// %f some form of float / decimal
+// %b bool
+// %A pretty print tuple, record types, union cases
+// %O obj.ToString()
+
+let myFunc arg1 arg2 = 
+    printfn "%b" arg1
+    0
+
+let helloWorld =
+    printfn "Hello World from %s!"
+
+[<EntryPoint>]
+let main argv =
+    argv |> Array.iter helloWorld
+    0 // return an integer exit code
